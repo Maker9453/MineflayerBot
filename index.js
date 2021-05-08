ad93554e49ea063f197a423c02995f7326792332
PeperSauce <n540513@e1.ntps.kh.edu.tw>
2021-05-08 18:34:45 +0800

    LookAtBot succeed
    Succeed!

---
+++ index.js
@@ -0,0 +1,20 @@
+const mineflayer = require("mineflayer")
+
+const bot = mineflayer.createBot({
+  host: "localhost",
+  port: 52876,
+  username: "PsauceBot",
+})
+
+function LookAtPlayer () {
+  const playerFilter = (entity) => entity.type == 'player'
+  const playerEntity = bot.nearestEntity(playerFilter)
+
+  if (!playerEntity) return
+
+  const pos = playerEntity.position.offset(0,playerEntity.height,0)
+  bot.lookAt(pos)
+
+}
+
+bot.on("physicTick", LookAtPlayer)
