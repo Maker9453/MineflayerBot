78beec19a3695f3c6f6f2393cc70408efe719af1
PeperSauce <n540513@e1.ntps.kh.edu.tw>
2021-05-11 22:25:19 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const minflayer = require("mineflayer")
 const bot = minflayer.createBot({
-  port: 62949,
+  port: 60618,
   host: "localhost",
   username: "PsauceBot",
 })
+const mcData = require("minecraft-data")(bot.version)
+const pos = bot.entity.position,global
+const theblock = bot.findBlock(options = {
+  x: pos.x,
+  y: pos.y - 1,
+  z: pos.z,
+}),global
 
+function MLG(){
+  bot.heldItem(mcData.items[661])
+  bot.activateItem()
+}
+if (theblock == mcData.blocks[0]){
+  MLG()
+}
--- index2.js
+++ index2.js
@@ -1,23 +1,23 @@
 
 const minflayer = require("mineflayer")
 const bot = minflayer.createBot({
-  port: 62331,
+  port: 60618,
   host: "localhost",
   username: "PsauceBot2",
 })
 
 bot.once("spawn",() => {
-  bot.mcData = require("minecraft-data")(bot.version)
+  mcData = require("minecraft-data")(bot.version)
 })
 
 bot.on("chat",(username,message) => {
   const args = message.split(' ')
   if (args[0] == "item_id") {
-    const item_id = bot.mcData.itemsByName[args[1]].id
+    const item_id = mcData.itemsByName[args[1]].id
     bot.chat("那物品ID為" + item_id )
   }
   if (args[0] == "block_id") {
-    const block_id = bot.mcData.blocksByName[args[1]].id
+    const block_id = mcData.blocksByName[args[1]].id
     bot.chat("那方塊ID為" + block_id )
   }
 })
