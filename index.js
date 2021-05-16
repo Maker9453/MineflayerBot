157f9c4350674c1b9ba382962f660530b7a84294
PeperSauce <n540513@e1.ntps.kh.edu.tw>
2021-05-16 23:06:03 +0800

    not really MLG

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
 // 基本資料庫
-const minflayer = require("mineflayer")
-
+const mineflayer = require("mineflayer")
+const Vec3 = require('vec3').Vec3
+ 
 // Bot 創建 與 MC資料庫
-const bot = minflayer.createBot({
-  port: 63150,
+const bot = mineflayer.createBot({
+  port: 63446,
   host: "localhost",
   username: "PsauceBot",
 })
-// 基本指令
-bot.on("spawn",MLG)
-// 函式集
-function MLG(){
-  const bot_pos = {
-    x: bot.entity.position.x,
-    y: bot.entity.position.y - 1,
-    z: bot.entity.position.z,
-  }
+
+bot.once("spawn",()=>{
   const mcData = require("minecraft-data")(bot.version)
-  // 函式
-  bot.heldItem = mcData.items[661]
-}
+  bot.on("physicTick",()=>{  
+    const pos = new Vec3(bot.entity.position.x,bot.entity.position.y - 1,bot.entity.position.z)
+    if (bot.blockAt(point = pos).name == mcData.blocks[0].name) {
+      bot.lookAt(point = pos)
+      bot.setQuickBarSlot(0)
+      if(bot.inventory.slots[36] == null){
+        for(let inventory_slots = 36; inventory_slots < 46; inventory_slots ++){
+          if(bot.inventory.slots[inventory_slots] == null) return
+          if (bot.inventory.slots[inventory_slots].name == mcData.items[661].name) {
+            bot.moveSlotItem(inventory_slots, 36)
+            bot.lookAt(point = pos)
+            bot.activateItem()
+            break
+          }else{
+            bot.lookAt(point = pos)
+            bot.activateItem()
+          }
+        }
+      }else{
+        if(bot.inventory.slots[36].name != "water_bucket"){
+          bot.chat("!= water_bucket")
+          for(let inventory_slots = 36; inventory_slots < 45; inventory_slots ++){
+            if(bot.inventory.slots[inventory_slots] == null) return
+            if (bot.inventory.slots[inventory_slots].name == mcData.items[661].name) {
+              bot.chat("move item")
+              bot.moveSlotItem(inventory_slots, 36)
+              bot.lookAt(point = pos)
+              bot.activateItem()
+              break
+            }
+          }
+        }else{
+          bot.chat("== water_bucket")
+          bot.lookAt(point = pos)
+          bot.activateItem()
+          setTimeout(() => {
+            bot.activateItem()
+          }, 100);
+          bot.chat("有放水")
+        }
+      }
+    }
+  })
+})
--- index2.js
+++ index2.js
@@ -1,25 +1,15 @@
 
 const minflayer = require("mineflayer")
 const bot = minflayer.createBot({
-  port: 63150,
+  port: 63446,
   host: "localhost",
   username: "PsauceBot2",
 })
 
-bot.once("spawn",() => {
-  const mcData = require("minecraft-data")(bot.version)
+bot.on("physicTick",()=>{
+  const helditem = bot.heldItem
+  console.log(helditem)
 })
 
-bot.on("chat",(username,message) => {
-  const args = message.split(' ')
-  if (args[0] == "item_id") {
-    const item_id = mcData.itemsByName[args[1]].id
-    bot.chat("那物品ID為" + item_id )
-  }
-  if (args[0] == "block_id") {
-    const block_id = mcData.blocksByName[args[1]].id
-    bot.chat("那方塊ID為" + block_id )
-  }
-})
 
 
