7bfe14175baaa302801b7980065511b02eaae704
PeperSauce <n540513@e1.ntps.kh.edu.tw>
2021-05-18 21:40:54 +0800

    finally

--- ask.js
+++ ask.js
@@ -1,14 +1,15 @@
 
 const minflayer = require("mineflayer")
+const Vec3 = require("vec3").Vec3
 const bot = minflayer.createBot({
-  port: 63446,
+  port: 50675,
   host: "localhost",
   username: "PsauceBot2",
 })
 
 bot.on("physicTick",()=>{
-  const helditem = bot.heldItem
-  console.log(helditem)
+  const pos = new Vec3(bot.entity.position.x,bot.entity.position.y - 1,bot.entity.position.z)
+
 })
 
 
