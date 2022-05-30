cca6a923dd2a6afdced95373e1ba12f4720b7a75
PeperSauce <n540513@e1.ntps.kh.edu.tw>
2022-05-30 21:46:07 +0800

    只能出生

--- index.js
+++ index.js
@@ -5,15 +5,3 @@ const bot = mineflayer.createBot({
   port: 60712,
   username: 'PsauceBot'
 })
-
-function lookAtNearestPlayer () {
-  const playerFilter = (entity) => entity.type === 'player'
-  const playerEntity = bot.nearestEntity(playerFilter)
-  
-  if (!playerEntity) return
-  
-  const pos = playerEntity.position.offset(0, playerEntity.height, 0)
-  bot.lookAt(pos)
-}
-
-bot.on('physicTick', lookAtNearestPlayer)
